a31e4758a38570fd852c2b6aa2a74fa4f0b1c170
raghu <subamraghu11154@gmail.com>
2024-02-06 14:34:31 +0530

    first

---
+++ Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Task
+    {
+        public void uc1()
+         {
+        Random random = new Random();
+        int attendance = random.Next(0, 2); // Generate a random number between 0 and 1
+
+        if (attendance == 1)
+        {
+            Console.WriteLine("Employee is present");
+        }
+        else if (attendance == 0)
+        {
+            Console.WriteLine("Employee is absent");
+        }
+    }
+    }
+}
