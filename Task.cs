bad0c49a4a55918c492659aaa9d84975ce97ae66
raghu <subamraghu11154@gmail.com>
2024-02-06 15:51:24 +0530

    completed for monthly wages

--- Task.cs
+++ Task.cs
@@ -2,54 +2,65 @@
 
 namespace Exceptions
 {
-   public class Task
+    public class Task
     {
+        private static int wagePerHour = 20;
+        private static int fullDayHour = 8;
+        private static int partTimeDayHour = 4;
+
         public void Uc1()
         {
-            Random random = new Random();
-            int attendance = random.Next(0, 2); // Generate a random number between 0 and 1
-
-            if (attendance == 1)
-            {
-                Console.WriteLine("Employee is present");
-                DailyEmployeeWage();
-            }
-            else if (attendance == 0)
-            {
-                Console.WriteLine("Employee is absent");
-            }
-        }
-
-        public void DailyEmployeeWage()
-        {
-            int fullTimeWagePerHour = 20;
-            int fullDayHour = 8;
-            if (EmployeeAttendance() == 1)
-            {
-                int dailyWage = fullTimeWagePerHour * fullDayHour;
-                Console.WriteLine($"The daily wage for a full-time employee is: {dailyWage}");
-            }
-            else
-            {
-                Console.WriteLine("The employee is not present");
-            }
-
-        }
-        public void PartTime()
-        {
-           
-            int partTimeWagePerHour = 20;
-            int partTimeDayHour = 8;
-
-                int partTimeDailyWage = partTimeWagePerHour * partTimeDayHour;
-                Console.WriteLine($"The daily wage for a part-time employee is: {partTimeDailyWage}");
-            
+            int monthlyWage = CalculateWagesOfMonth();
+            Console.WriteLine($"The monthly wage of the employee is: ${monthlyWage}");
         }
 
         public int EmployeeAttendance()
         {
             Random random = new Random();
-            return random.Next(0, 2); // Generate a random number between 0 and 1
+            return random.Next(0, 3); // Generate a random number between 0 and 2
+        }
+
+        public int DailyEmployeeWage(int attendanceStatus)
+        {
+            if (attendanceStatus == 1)
+            {
+                return wagePerHour * fullDayHour;
+            }
+            else if (attendanceStatus == 2)
+            {
+                return wagePerHour * partTimeDayHour;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+
+        public void PartTime()
+        {
+
+            int partTimeWagePerHour = 20;
+            int partTimeDayHour = 8;
+
+            int partTimeDailyWage = partTimeWagePerHour * partTimeDayHour;
+            Console.WriteLine($"The daily wage for a part-time employee is: {partTimeDailyWage}");
+
+        }
+
+        public int CalculateWagesOfMonth()
+        {
+            int numberOfWorkingDays = 20;
+            int monthlyWage = 0;
+
+            for (int i = 0; i < numberOfWorkingDays; i++)
+            {
+                int dailyWage = DailyEmployeeWage(EmployeeAttendance());
+                monthlyWage += dailyWage;
+            }
+
+            return monthlyWage;
         }
     }
+
 }
